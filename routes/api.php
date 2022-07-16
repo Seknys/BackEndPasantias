c51146c91e793abf83f95fef642b8036ebbc4cd0
Steven Guañuna <74793607+Seknys@users.noreply.github.com>
2022-07-15 19:16:53 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/user',[ApiController::class, 'index']); //Permite mostrar todos los usuarios y una respuesta personalizada con el status y code
+Route::get('/user',[ApiController::class, 'index'])->name("index"); //Permite mostrar todos los usuarios y una respuesta personalizada con el status y code
 Route::get('/user/{id}',[ApiController::class, 'show']);//Muestra un usuario en especifico dependiendo del id ingresado, si no lo encuentra muestra error 400
 Route::get('/users/{id}',[ApiController::class, 'position']);//Muestra todos los usuarios dependiendo de la posicion ingresada, si no la encuentra muestra un array vacio y error 400
